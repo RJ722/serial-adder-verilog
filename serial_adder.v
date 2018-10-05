1908b82673009151c0a239c8e85637f8ec90ae28
RJ722 <rahul722j@gmail.com>
2018-10-05 23:32:26 +0530

    Add support for visualising carry bit

--- serial_adder.v
+++ serial_adder.v
@@ -2,20 +2,22 @@
 `include "d_flipflop.v"
 `include "full_adder.v"
 
-module serial_adder(data_a, data_b, clk, reset, out);
+module serial_adder(data_a, data_b, clk, reset, out, cout);
   input [3:0] data_a, data_b;
   input clk, reset;
+  output cout;
   output [3:0] out;
 
   reg [3:0] out;
   reg [2:0] count;
-  reg enable;
-  wire wire_a, wire_b, cin, cout, sum;
+  reg enable, cout;
+  wire wire_a, wire_b, cout_temp, cin, sum;
 
   piso piso_a(clk, enable, reset, data_a, wire_a);
   piso piso_b(clk, enable, reset, data_b, wire_b);
-  full_adder adder(wire_a, wire_b, cin, sum, cout);
+  full_adder adder(wire_a, wire_b, cin, sum, cout_temp);
   d_flipflop dff(cout, clk, enable, reset, cin);
+
 
   always @ (posedge clk or posedge reset) begin
     if (reset) begin
@@ -26,6 +28,7 @@ module serial_adder(data_a, data_b, clk, reset, out);
         enable = 0;
       else begin
         if (enable) begin
+          cout = cout_temp;
           count = count + 1;
           out = out >> 1;
           out[3] = sum;
--- serial_adder_tb.v
+++ serial_adder_tb.v
@@ -6,16 +6,17 @@ module serial_adder_tb;
   reg clk, reset;
 
   wire [3:0] out;
+  wire cout;
 
-  serial_adder s_adder(data_a, data_b, clk, reset, out);
+  serial_adder s_adder(data_a, data_b, clk, reset, out, cout);
 
   initial begin
     $monitor("data_a = %4b, data_b = %4b, reset = %b, out=%b", data_a, data_b, reset, out);
     $dumpfile("serial_adder_tb.vcd");
     $dumpvars(0, serial_adder_tb);
     clk = 0;
-    data_a = 4'b1101; data_b = 4'b0001; reset = 1; #30;
-    data_a = 4'b1101; data_b = 4'b0001; reset = 0; #200;
+    data_a = 4'b1111; data_b = 4'b1111; reset = 1; #20;
+    data_a = 4'b1111; data_b = 4'b1111; reset = 0; #200;
     $finish;
   end
 
